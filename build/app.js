d3c9193d57a59558c49912af64f97804eaaec1f5
CamilaCarrenoV <camila.carreno89@gmail.com>
2024-10-01 17:25:35 -0300

    comenzando con paramentros de endpoint

---
+++ build/app.js
@@ -0,0 +1,27 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const client_1 = require("@prisma/client");
+const app = (0, express_1.default)();
+const prisma = new client_1.PrismaClient();
+const PORT = 3001;
+app.use('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const taxis = yield prisma.taxis.findMany();
+    res.json(taxis);
+}));
+app.listen(PORT, () => {
+    console.log('SERVER IS UP ON PORT:', PORT);
+});
+
